dc23fd34efd9c22106c1fa1427f23591d0093642
Kunal Kendrekar <M1054824@mindtree.com>
2020-04-01 17:41:55 +0530

    fix initial setup

--- src/app.js
+++ src/app.js
@@ -5,15 +5,14 @@ const express = require("express")
 const app = express()
 
 app.set('views', path.join(__dirname, '/views'))
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.set('view engine', 'ejs');
 
 
 app.get('/', function (req, res) {
-    res.render('index', { title: 'index' })
+    res.render('index', { title: 'Index' })
 })
 
-app.listen(3000)
+app.listen(3000, function () { console.log('PS Project running on port 3000!') })
 
-console.log('PS portal running on port 3000!')
