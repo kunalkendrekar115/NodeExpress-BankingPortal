04ac3001a8992c354c61aa4df26492a2dc05544c
Kunal Kendrekar <M1054824@mindtree.com>
2020-04-01 18:13:58 +0530

    Use Express to create Server

--- src/app.js
+++ src/app.js
@@ -9,9 +9,30 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.set('view engine', 'ejs');
 
+const accountData = fs.readFileSync(path.join(__dirname, '/json/accounts.json'), "UTF8")
+const accounts = JSON.parse(accountData)
+
+const userData = fs.readFileSync(path.join(__dirname, '/json/users.json'), "UTF8")
+const users = JSON.parse(userData)
+
+app.get('/savings', function (req, res) {
+    res.render('account', { account: accounts.savings })
+})
+
+app.get('/checking', function (req, res) {
+    res.render('account', { account: accounts.checking })
+})
+
+app.get('/credit', function (req, res) {
+    res.render('account', { account: accounts.credit })
+})
+
+app.get('/profile', function (req, res) {
+    res.render('profile', { user: users[0] })
+})
 
 app.get('/', function (req, res) {
-    res.render('index', { title: 'Index' })
+    res.render('index', { title: 'Account Summary', accounts: accounts })
 })
 
 app.listen(3000, function () { console.log('PS Project running on port 3000!') })
